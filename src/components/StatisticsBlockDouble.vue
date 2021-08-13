b66e3b7e09119dece142c8ec80d7e32ca2573526
Adrian Sian <adriansian@gmail.com>
2021-08-13 18:19:49 +0800

    is covid over: make it more bolder

--- src/components/StatisticsBlockDouble.vue
+++ src/components/StatisticsBlockDouble.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="statistics-div">
     <h4 class="heading-h4">{{ Title }}</h4>
-    <span class="number-span  mr-3" style="font-weight: 500;">Today: {{ Today.toLocaleString() }}</span>
+    <span class="number-span  mr-3" style="font-weight: 600;">Today: {{ Today.toLocaleString() }}</span>
     <span class="number-span">Total: {{ Total.toLocaleString() }}</span>
 
   </div>
