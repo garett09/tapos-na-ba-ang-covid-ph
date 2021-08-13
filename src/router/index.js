1a62e8a0322677b748a4c37bc0f23842fabf9a71
Adrian Sian <adriansian@gmail.com>
2021-08-13 17:04:25 +0800

    is covid over: fix routing

--- src/router/index.js
+++ src/router/index.js
@@ -7,20 +7,29 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue")
+    component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue"),
+    meta: {
+      title: "Tapos na ba?"
+    }
   },
   {
     path: "/about",
     name: "About",
-    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    meta: {
+      title: "Tungkol sa Proyekto na ito"
+    }
   },
-  
+
   // Error routes
   {
     path: "/404",
     name: "NotFound",
     component: () =>
-      import(/* webpackChunkName: "notfound" */ "../views/errors/404.vue")
+      import(/* webpackChunkName: "notfound" */ "../views/errors/404.vue"),
+    meta: {
+      title: "404 Page Not Found"
+    }
   },
   // Catch all route which redirects to the 404 page
   {
@@ -34,5 +43,12 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 });
+
+// Set the page title per route dynamicly
+router.beforeEach((toRoute, fromRoute, next) => {
+  window.document.title = toRoute.meta && toRoute.meta.title ? toRoute.meta.title : "Tapos na ba?";
+
+  next();
+})
 
 export default router;
