251b0526cfb3205ed2493ccc68df832b0b87905d
Adrian Sian <adriansian@gmail.com>
2021-08-13 17:31:18 +0800

    is covid over: add locale format

--- src/components/StatisticsBlockDouble.vue
+++ src/components/StatisticsBlockDouble.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="statistics-div">
     <h4 class="heading-h4">{{ Title }}</h4>
-    <span class="number-span mr-3">Total: {{ Total }}</span>
-    <span class="number-span">Today: {{ Today }}</span>
+    <span class="number-span mr-3">Total: {{ Total.toLocaleString() }}</span>
+    <span class="number-span">Today: {{ Today.toLocaleString() }}</span>
   </div>
 </template>
 
